882263fb7f4374b2a74004d872471cf3db261647
AsusSamat <bagishbekov113@gmail.com>
2021-12-03 12:37:22 +0600

    free

---
+++ app/src/main/java/com/geektech/freetaskapp/ui/fragments/profile/ProfileFragment.java
@@ -0,0 +1,32 @@
+package com.geektech.freetaskapp.ui.fragments.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.geektech.freetaskapp.databinding.FragmentProfileBinding;
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
